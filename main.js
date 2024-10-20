afd706e0892b655878b4d69f6009e5908b670e80
Nguyen063 <phamvannguyen063@gmail.com>
2024-10-20 13:13:24 +0700

    ok
    ok

---
+++ main.js
@@ -0,0 +1,7 @@
+window.onload = () => {
+  // Khởi tạo hiệu ứng nở hoa sau khi tải trang
+  setTimeout(() => {
+    document.body.classList.remove("not-loaded");
+  }, 1000); // Hiệu ứng sẽ bắt đầu sau 1 giây
+  
+};
